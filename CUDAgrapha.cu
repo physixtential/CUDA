9e2f4ca5aefe4bd3b7008d3d8b43733e4602bb22
physixtential <55558141+physixtential@users.noreply.github.com>
2020-11-05 12:08:00 -0800

    CUDA graph

---
+++ CUDAgrapha.cu
@@ -0,0 +1,16 @@
+// Define graph of work + dependencies
+
+cudaGraphCreate(&graph);
+cudaGraphAddNode(graph, kernel_a, {}, ...);
+cudaGraphAddNode(graph, kernel_b, { kernel_a }, ...);
+cudaGraphAddNode(graph, kernel_c, { kernel_a }, ...);
+cudaGraphAddNode(graph, kernel_d, { kernel_b, kernel_c }, ...);
+
+// Instantiate graph and apply optimizations
+
+cudaGraphInstantiate(&instance, graph);
+
+// Launch executable graph 100 times
+
+for(int i=0; i<100; i++)
+      cudaGraphLaunch(instance, stream);
